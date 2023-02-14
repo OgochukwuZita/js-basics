42b916048219ce15e05b573de06d0c33c3754c04
Ogbonna Ogochukwu <ogochukwuzita86@gmail.com>
2023-02-14 12:48:43 +0100

    completed variable question

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-console.log ('stutern cohort 1.4')
-//function that multiplies two numbers
+console.log('stutern cohort 1.4')
+// function that multiplies two numbers
 
-function mul(number1, number2){
-    return 2*2;
+function mul(number1, number2) {
+    return 2 * 2;
 }
-console.log (mul (2*2))
+console.log(mul(2 * 2))
+
+// variables
+
+ let fName, lName, track, skills, color, independence ;
+ fName = 'Ogochukwu';
+ lName = 'Ogbonna';
+ track = 'frontend development';
+ skills = 'html, CSS and git';
+ color = 'purple';
+ independence = '1960';
+
+ console.log(fName);
+ console.log (lName);
+ console.log (track);
+ console.log(skills);
+ console.log(color);
+ console.log(independence);
+
+ //concatenation
+
+ console.log("My name is " + lName + " " + fName + ". My track is " + track + ". The skills I have acquired are, " + skills + ". My favorite color is " + color + ". My country Nigeria, gained her independence in the year, " + independence + ".")
